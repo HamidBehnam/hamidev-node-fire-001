5d742923eb7ea71151ac167d354a3f665601e3e1
HamidBehnam <hamid.behnam@gmail.com>
2019-12-22 18:29:15 -0800

    using the default configuration settings to initialize the firebase app

--- functions/src/common/firebase.service.ts
+++ functions/src/common/firebase.service.ts
@@ -1,9 +1,8 @@
 import * as admin from 'firebase-admin';
-import functions = require("firebase-functions");
 import Firestore = FirebaseFirestore.Firestore;
 import Auth = admin.auth.Auth;
 
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 
 export const db: Firestore = admin.firestore();
 
